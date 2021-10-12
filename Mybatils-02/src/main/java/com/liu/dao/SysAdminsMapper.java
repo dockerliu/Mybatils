b4186278f5315a788a79c1a165b86457323ec0bd
DockerLiu <DockerLiu2019@gmail.com>
2021-10-12 23:41:33 +0100

    另一个工程

---
+++ Mybatils-02/src/main/java/com/liu/dao/SysAdminsMapper.java
@@ -0,0 +1,28 @@
+package com.liu.dao;
+
+import com.liu.pojo.SysAdmins;
+
+import java.util.List;
+import java.util.Map;
+
+//表接口方法
+public interface SysAdminsMapper {
+    //获取全部用户
+    List<SysAdmins> getSysAdminList();
+
+    //根据ID查询用户
+    SysAdmins getSysAdminById(int adminId);
+
+
+    //Insert一个用户
+    int addUser(SysAdmins admin);
+
+    //模糊查询
+    List<SysAdmins> getSysAdminLikeName(String name);
+
+    //修改用户
+    int updateUser(SysAdmins admin);
+
+    //删除用户
+    int deleteUser(int adminId);
+}
---
+++ Mybatils-02/src/main/java/com/liu/utils/MybatisUtils.java
@@ -0,0 +1,37 @@
+package com.liu.utils;
+
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+//sqlSessionFactory --> sqlSession(数据库连接通用类)
+public class MybatisUtils {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    /* 加静态sqlSession资源 */
+    static {
+
+        try {
+            /* 使用Mybatis第一步：获取sqlSessionFactory对象 */
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* 返回sqlSession对象 */
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
+
+    }
+
+}
